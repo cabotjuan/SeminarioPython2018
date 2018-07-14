3c9842002bd03f7a1bab35552e21dd51e17e51ba
daiana.ruiz <daiana.ruiz.1996@gmail.com>
2018-07-14 15:12:38 -0300

    arrastrar solucionado

--- comeVocales2.py
+++ comeVocales2.py
@@ -20,7 +20,10 @@ def cargarVocales(L_Vocales):
 		pantalla.blit(nuevoItem.image, nuevoItem.rect)
 	
 ## CARGAR LA IMG DE LOS OBJETOS A ARRASTRAR.
-def cargarObjetos(L_Objetos, L_img):
+def cargarObjetos(L_Objetos):
+	
+	L_img = os.listdir('Imagenes/comienzanConVocal')
+	
 	despl_x = 10
 	for j in range(CANT_IMAGENES_PANTALLA):
 		i = random.randrange(0, len(L_img)-1)
@@ -30,49 +33,95 @@ def cargarObjetos(L_Objetos, L_img):
 		nuevoItem.setX(despl_x)
 		nuevoItem.setY(V_LARGO/3)		
 		L_Objetos.append(nuevoItem)
-		pantalla.blit(nuevoItem.image, nuevoItem.rect)
+		#pantalla.blit(nuevoItem.image, nuevoItem.rect)
 		del L_img[i]
 
 def main():
 	fin_juego = False
-	L_img = os.listdir('Imagenes/comienzanConVocal')
+	
 	
 	L_Vocales = []
 	L_Objetos = []
 	
+	pantalla.fill((50,50,50))
 	cargarVocales(L_Vocales)
-	cargarObjetos(L_Objetos, L_img)
+	p_base = pantalla.copy()
+	cargarObjetos(L_Objetos)
 	pygame.display.update()
 	seleccionado = None
+	
+	for i in range(len(L_Objetos)):
+	
+		pantalla.blit(L_Objetos[i].image, L_Objetos[i].rect)
+	
+	p_conObj = pantalla.copy()	
+	
 	## LOOP PRINCIPAL
+	
 	while True:
+		
 		Qeventos = pygame.event.get()
 		for evento in Qeventos:
 			if evento.type == QUIT:
 				pygame.quit()
 				sys.exit()
 			elif evento.type == MOUSEBUTTONDOWN and evento.button == 1:
-				if L_Objetos[0].rect.collidepoint((evento.pos[0],evento.pos[1])):
-					seleccionado = L_Objetos[0]
-				if L_Objetos[1].rect.collidepoint((evento.pos[0],evento.pos[1])):
-					seleccionado = L_Objetos[1]
-				if L_Objetos[2].rect.collidepoint((evento.pos[0],evento.pos[1])):
-					seleccionado = L_Objetos[2]
-				if L_Objetos[3].rect.collidepoint((evento.pos[0],evento.pos[1])):
-					seleccionado = L_Objetos[3]
+				
+				
+				for i in range(0, len(L_Objetos)):
+					
+					if L_Objetos[i].rect.collidepoint((evento.pos[0],evento.pos[1])):
+						seleccionado = L_Objetos[i]
+						ind_obj = i
+					
+				if seleccionado:
+					copia_x = seleccionado.getX()
+					copia_y = seleccionado.getY()
 				print('se selecciono obj')
 					
 			elif evento.type == MOUSEBUTTONUP and evento.button == 1:
+			
+				if seleccionado:
+				
+					index = seleccionado.rect.collidelist(L_Vocales) 
+					
+				
+					
+					if index != -1 and L_Vocales[index].vocal == seleccionado.vocal:
+						#===>Contar puntos aca<===
+						print ('borrado'+str(L_Objetos.index(seleccionado)))
+						del L_Objetos[L_Objetos.index(seleccionado)]
+						if not L_Objetos:
+							cargarObjetos(L_Objetos)	
+						
+					else:
+						seleccionado.setX(copia_x)
+						seleccionado.setY(copia_y)
+						
+				pantalla.blit(p_base,(0,0))		
+						
+				for i in range(0, len(L_Objetos)):
+					
+					pantalla.blit(L_Objetos[i].image, L_Objetos[i].rect)
+				p_conObj = pantalla.copy()	
+
 				seleccionado = None
+			
+						
+					
 				
 				print('se levanta el mouse')
 				
 			elif evento.type == MOUSEMOTION and seleccionado:
+				
+				pantalla.blit(p_conObj, (0,0))
 				seleccionado.rect.centerx = evento.pos[0]
 				seleccionado.rect.centery = evento.pos[1]
 				pantalla.blit(seleccionado.image, seleccionado.rect)
-				print('x'+str(seleccionado.getX())+'y'+str(seleccionado.getY()))
 				print('arrastrando..')
+				
+				
+			
 		pygame.display.update()
 		
 

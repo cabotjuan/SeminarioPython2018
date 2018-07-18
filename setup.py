c41f0d9f489be9ebd91e2122033b470f4edfa798
Cabot, Juan Andres <j_cabot@live.com.ar>
2018-07-18 14:28:44 -0300

    Agrego sonidos

--- setup.py
+++ setup.py
@@ -7,3 +7,8 @@ V_LARGO = 740
 
 pantalla = pygame.display.set_mode((V_ANCHO,V_LARGO))
 
+pygame.mixer.init()
+#Sonidos 
+S_Click = pygame.mixer.Sound('Sonidos/Click.wav')
+S_Correcto = pygame.mixer.Sound('Sonidos/Correcto.wav')
+S_Incorrecto = pygame.mixer.Sound('Sonidos/Incorrecto.wav')
